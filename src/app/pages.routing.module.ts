519b776b7cf31cd238889c423188b54a3a52da67
Jotta-Art <joao.pedro42@outlook.com.br>
2023-06-09 00:42:15 -0300

    Ajustando sidebar

--- src/app/pages.routing.module.ts
+++ src/app/pages.routing.module.ts
@@ -3,9 +3,25 @@ import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
     {
+        path: 'pagina-inicial', loadChildren: () =>
+            import('./pagina-inicial/pagina-inicial.module').then(mod => mod.PaginaInicialModule)
+    },
+    {
         path: 'login', loadChildren: () =>
             import('./login/login.module').then(mod => mod.LoginModule)
     },
+    // {
+    //     path: 'sobre', loadChildren: () =>
+    //         import('./sobre/sobre.module').then(mod => mod.LoginModule)
+    // },
+    // {
+    //     path: 'servicos', loadChildren: () =>
+    //         import('./login/login.module').then(mod => mod.LoginModule)
+    // },
+    // {
+    //     path: 'contato', loadChildren: () =>
+    //         import('./login/login.module').then(mod => mod.LoginModule)
+    // }
 ];
 
 @NgModule({ imports: [RouterModule.forChild(routes)], exports: [RouterModule] })
